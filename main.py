f6762ab56475d3484e475c2a554c96585dfaa09c
didik Setiawan <didik2103117@gmail.com>
2023-03-13 00:04:54 +0700

    final

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ from tkinter import messagebox
 from math import pi, e, sin, cos, tan, log
 
 window = Tk()
-screenSize = "279x442"
+screenSize = "300x452"
 window.geometry(screenSize)
 window.resizable(0, 0)
-window.title("Calcualtor")
+window.title("Kalkulator")
 #function
 def about():
-    messagebox.showinfo('About',"\n \n \n   tugas kalkulator   \n didik Setiawan \n XII RPL 2 absen 33 \n")
+        messagebox.showinfo('About',"\n \n \n   tugas kalkulator   \n didik Setiawan \n XII RPL 2 absen 33 \n")
 
 def clickButton(item):
     global expression
@@ -23,11 +23,7 @@ def clearButton():
 def clearAll():
     inputText.set("")
 
-def expand():
-    if screenSize=="279x500":
-        window.geometry("279x555")
-    else:
-        window.geometry("279x555")
+
 def equalButton():
     result = ""
     try:
@@ -59,7 +55,7 @@ inputField = Entry(inputFrame, font=('arial', 25, ), textvariable=inputText, wid
 inputField.grid(row=0, column=0)
 inputField.pack(ipady=13)
 
-mainFrame = Frame(window, width=312, height=272.5, bg="black")
+mainFrame = Frame(window, width=412, height=272.5, bg="black")
 mainFrame.pack()
 
 ac = PhotoImage(file = r"images\ac.png")
@@ -72,15 +68,21 @@ clearimage = clear.subsample(4,4)
 clear = Button(mainFrame, text="AC", fg="black", image=clearimage, bd=0, bg="black", cursor="hand1",
                command=lambda: clearButton()).grid(row=0, column=1, padx=1, pady=1)
 
+kuadrat = PhotoImage(file = r"images\kuadrat.png")
+kuadratimage = kuadrat.subsample(4,4)
+kuadrat = Button(mainFrame, text="..2", fg="black", image=kuadratimage, bd=0, bg="black", cursor="hand2",
+               command=lambda: clickButton("**2")).grid(row=0, column=2, padx=1, pady=1)
+
+
 divide = PhotoImage(file = r"images\divide.png")
 divideimage = divide.subsample(4,4)
 divide = Button(mainFrame, text="/", fg="white",image=divideimage, bd=0, bg="black", cursor="hand2",
-                command=lambda: clickButton("/")).grid(row=0, column=2, padx=1, pady=1)
+                command=lambda: clickButton("/")).grid(row=0, column=3, padx=1, pady=1)
 
 
-seven = PhotoImage(file = r"images\seven.png")
-sevenimage = seven.subsample(4,4)
-seven = Button(mainFrame, text="7", fg="black", image=sevenimage, bd=0, bg="black", cursor="hand2",
+test = PhotoImage(file = r"images\seven.png")
+testimage = test.subsample(4,4)
+test = Button(mainFrame, text="7", fg="black", image=testimage, bd=0, bg="black", cursor="hand2",
                command=lambda: clickButton(7)).grid(row=1, column=0, padx=1, pady=1)
 
 eight = PhotoImage(file = r"images\eight.png")
@@ -96,7 +98,7 @@ nine = Button(mainFrame, text="9", fg="black", image=nineimage, bd=0, bg="black"
 multi = PhotoImage(file = r"images\multi.png")
 multiimage = multi.subsample(4,4)
 multiply = Button(mainFrame, text="*", fg="white",image=multiimage, bd=0, bg="black", cursor="hand2",
-                  command=lambda: clickButton("*")).grid(row=0, column=3, padx=1, pady=1)
+                  command=lambda: clickButton("*")).grid(row=1, column=3, padx=1, pady=1)
 
 four = PhotoImage(file = r"images\four.png")
 fourimage = four.subsample(4,4)
@@ -116,7 +118,7 @@ six = Button(mainFrame, text="6", fg="black", image=siximage, bd=0, bg="black", 
 minus = PhotoImage(file = r"images\minus.png")
 minusimage = minus.subsample(4,4)
 minus = Button(mainFrame, text="-", fg="white",image=minusimage, bd=0, bg="black", cursor="hand2",
-               command=lambda: clickButton("-")).grid(row=1, column=3, padx=1, pady=1)
+               command=lambda: clickButton("-")).grid(row=2, column=3, padx=1, pady=1)
 
 one = PhotoImage(file = r"images\one.png")
 oneimage = one.subsample(4,4)
@@ -136,24 +138,29 @@ three = Button(mainFrame, text="3", fg="black",image=threeimage, bd=0, bg="black
 plus = PhotoImage(file = r"images\plus.png")
 plusimage = plus.subsample(4,4)
 plus = Button(mainFrame, text="+", fg="white",image=plusimage, bd=0, bg="black", cursor="hand2",
-              command=lambda: clickButton("+")).grid(row=2, column=3, padx=1, pady=1)
+              command=lambda: clickButton("+")).grid(row=3, column=3, padx=1, pady=1)
 
 
 zero = PhotoImage(file = r"images\0.png")
 zeroimage = zero.subsample(4,4)
 zero = Button(mainFrame, text="0", fg="black",image=zeroimage, bd=0, bg="black", cursor="hand2",
-              command=lambda: clickButton(0)).grid(row=4, column=0, columnspan=2, padx=1, pady=1)
+              command=lambda: clickButton(0)).grid(row=4, column=1, padx=1, pady=1)
+
+zero0 = PhotoImage(file = r"images\02.png")
+zero0image = zero0.subsample(4,4)
+zero0 = Button(mainFrame, text="00", fg="black",image=zero0image, bd=0, bg="black", cursor="hand2",
+              command=lambda: clickButton("00")).grid(row=4, column=2, padx=1, pady=1)
 
 point = PhotoImage(file = r"images\point.png")
 pointimage = point.subsample(4,4)
 point = Button(mainFrame, text=".", fg="black",image=pointimage, bd=0, bg="black", cursor="hand2",
-               command=lambda: clickButton(".")).grid(row=4, column=3, padx=1, pady=1)
+               command=lambda: clickButton(".")).grid(row=4, column=0, padx=1, pady=1)
 
 
 equal = PhotoImage(file = r"images\equal.png")
 equalimage = equal.subsample(4,4)
 equals = Button(mainFrame, text="=", image=equalimage, fg="white", bd=0, bg="black", cursor="hand2",
-                command=lambda: equalButton()).grid(row=3, column=3, padx=1, pady=1)
+                command=lambda: equalButton()).grid(row=4,column=3, padx=1, pady=1)
 
 window.iconbitmap("images\icon.ico")
 window.config(bg="black",menu=menubar)
